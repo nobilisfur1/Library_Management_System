faac8338e5d9c1bb79432d9b83c8cc994c7339e1
nobilis <kevinbaxterperkins@gmail.com>
2025-07-03 23:05:53 -0500

    removed run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Script to compile and run library management program.
-
-
-javac -cp libs/sqlite-jdbc-3.50.1.0.jar -d out src/**/*.java
-java -cp "libs/sqlite-jdbc-3.50.1.0.jar:out" Main
-
